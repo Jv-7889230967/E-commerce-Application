05cece2105d6c0179fc3e379f8f6608da56e709d
Jv-7889230967 <87749796+Jv-7889230967@users.noreply.github.com>
2023-09-08 18:07:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const connectDB=require("./config/DB");
 const Userroutes=require("./Routes/Userroutes");
 const cors=require('cors');
 connectDB();
+const port=process.env.PORT || 6000;
 
 app.use(cors());
 app.use(express.json());
 app.use("/user",Userroutes);
-app.listen(5000,()=>{
-    console.log("server connected");
+app.listen(port,()=>{
+    console.log(`server connected at ${port}`);
 })
